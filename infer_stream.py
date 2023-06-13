91012c1c20800d64b38a1c5f833a7a0d7f3d74f0
Siladittya Manna <sadimanna@users.noreply.github.com>
2023-06-13 18:23:46 +0530

    Update infer_stream.py

--- infer_stream.py
+++ infer_stream.py
@@ -92,11 +92,11 @@ if __name__ == '__main__':
         parser.add_argument('--pooler_mode', type=str, choices=Pooler.OPTIONS, help='default: {.value:s}'.format(Config.POOLER_MODE))
         parser.add_argument('--rpn_pre_nms_top_n', type=int, help='default: {:d}'.format(Config.RPN_PRE_NMS_TOP_N))
         parser.add_argument('--rpn_post_nms_top_n', type=int, help='default: {:d}'.format(Config.RPN_POST_NMS_TOP_N))
-        parser.add_argument('input', type=str, help='path to input stream endpoint')
-        parser.add_argument('period', type=int, help='period of inference')
+        parser.add_argument('--stream_input', type=str, help='path to input stream endpoint')
+        parser.add_argument('--period', type=int, help='period of inference')
         args = parser.parse_args()
 
-        path_to_input_stream_endpoint = args.input
+        path_to_input_stream_endpoint = args.stream_input
         period_of_inference = args.period
         dataset_name = args.dataset
         backbone_name = args.backbone
